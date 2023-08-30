c4860e8d79508a0aadeed727547b46924253cdd0
ScarsIntoStars <conpana_jj@naver.com>
2023-08-30 19:31:55 +0900

    DTO생성 및 main 생성

---
+++ src/dto/AccountDTO.java
@@ -0,0 +1,63 @@
+package dto;
+
+public class AccountDTO {
+    private Integer id;
+    private String accountNumber;
+    private int deposit;
+    private int withdraw;
+    private String bankingAt;
+
+    public AccountDTO() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getDeposit() {
+        return deposit;
+    }
+
+    public void setDeposit(int deposit) {
+        this.deposit = deposit;
+    }
+
+    public int getWithdraw() {
+        return withdraw;
+    }
+
+    public void setWithdraw(int withdraw) {
+        this.withdraw = withdraw;
+    }
+
+    public String getBankingAt() {
+        return bankingAt;
+    }
+
+    public void setBankingAt(String bankingAt) {
+        this.bankingAt = bankingAt;
+    }
+
+    @Override
+    public String toString() {
+        return "AccountDTO{" +
+                "id=" + id +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", deposit=" + deposit +
+                ", withdraw=" + withdraw +
+                ", bankingAt='" + bankingAt + '\'' +
+                '}';
+    }
+}
---
+++ src/dto/ClientDTO.java
@@ -0,0 +1,73 @@
+package dto;
+
+public class ClientDTO {
+    private Integer id;
+    private String clientName;
+    private String accountNumber;
+    private String clientPass;
+    private String clientCreatedAt;
+    private Integer balance;
+
+    public ClientDTO(Integer id) {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public String getClientPass() {
+        return clientPass;
+    }
+
+    public void setClientPass(String clientPass) {
+        this.clientPass = clientPass;
+    }
+
+    public String getClientCreatedAt() {
+        return clientCreatedAt;
+    }
+
+    public void setClientCreatedAt(String clientCreatedAt) {
+        this.clientCreatedAt = clientCreatedAt;
+    }
+
+    public Integer getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Integer balance) {
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "ClientDTO{" +
+                "id=" + id +
+                ", clientName='" + clientName + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", clientPass='" + clientPass + '\'' +
+                ", clientCreatedAt='" + clientCreatedAt + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+}
